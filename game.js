f7e036582c3c9d0933da14abad88007d4a75fc25
Amir Sejdinovic <amir24V@yahoo.com>
2021-02-03 10:35:01 +0100

    Creating trinagle on the screen

--- game.js
+++ game.js
@@ -13,8 +13,29 @@ document.body.prepend(canvas);
 //Init of canvas objact
 const ctx = canvas.getContext('2d');
 
+
+
 //Calling the function
-draw();
+drawpath();
+
+//Function for red triangle
+function drawpath(){
+   ctx.fillStyle = 'blue';
+   ctx.fillRect(canvas.width/2,canvas.height/2,5,5);
+   ctx.fillRect(100,100,5,5);
+   ctx.fillRect(100,300,5,5);
+   ctx.fillStyle = 'red';
+   ctx.beginPath();
+   ctx.moveTo(canvas.width/2, canvas.height/2);
+   ctx.lineTo(100, 100);
+   ctx.lineTo(100, 300);
+   ctx.lineTo(canvas.width/2, canvas.height/2);
+   ctx.stroke();
+   ctx.fill();
+
+
+
+}
 
 //Creating the function
 function draw(){
