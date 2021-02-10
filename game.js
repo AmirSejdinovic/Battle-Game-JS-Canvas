9c9a5a0c0c84cf676eb0c710510d3d3be5afcfc0
Amir Sejdinovic <amir24V@yahoo.com>
2021-02-10 11:30:07 +0100

    Adding the bullets and fire trigers

--- game.js
+++ game.js
@@ -18,19 +18,28 @@ const players = [
    y:canvas.height/2,
    size:30,
    speed:5,
-   color:'red'},
+   color:'red',
+   cooldown: 0,
+},
    {
    x:canvas.width/2 - (grid*2),
    y:canvas.height/2,
    size:30,
    speed:5,
-   color:'blue'}
+   color:'blue',
+   cooldown: 0
+}
 
 
 ];
 
 
-const game  = {req:''};
+const game  = {
+   req:'',
+   bullets:[],
+   bulletSpeed:5
+
+};
 
 const keyz = {
    ArrowLeft:false,
@@ -48,6 +57,27 @@ document.addEventListener('keydown',(e)=>{
    console.log(e);
    if(e.code in keyz){
       keyz[e.code] = true;
+   }
+   if(e.code == 'Space' && players[0].cooldown <= 0){
+      players[0].cooldown = 20;
+       game.bullets.push({
+          x:players[0].x - players[0].size -15,
+          y:players[0].y - 5,
+          speed: -game.bulletSpeed,
+          size: 10,
+          color: 'pink'
+       })
+      }
+
+       if(e.code == 'KeyD' && players[1].cooldown <= 0){
+         players[1].cooldown = 20;
+          game.bullets.push({
+             x:players[1].x + players[1].size +15,
+             y:players[1].y - 5,
+             speed: game.bulletSpeed,
+             size: 10,
+             color: 'lightblue'
+          })
    }
    
    //console.log(keyz);
@@ -89,12 +119,23 @@ function draw(){
    //console.log(player.x);
     movmentPlayer();
 
+    game.bullets.forEach((bull, index)=>{
+       ctx.fillStyle = bull.color;
+       ctx.fillRect(bull.x, bull.y,bull.size,bull.size);
+       bull.x+= bull.speed;
+       if(bull.x<0){
+           game.bullets.splice(index, 1);
+       }
+
+    })
+
     ctx.beginPath();
     ctx.moveTo(canvas.width/2,0);
     ctx.lineTo(canvas.width/2,canvas.height);
     ctx.stroke();
 
    players.forEach((player)=>{
+      if(player.cooldown > 0){player.cooldown--;}
       ctx.beginPath();
    ctx.fillStyle = player.color;
    ctx.arc(player.x,player.y,player.size,0,Math.PI*2);
